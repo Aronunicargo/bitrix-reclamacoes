758ed2357967af5c7dec2455eaae941ef71250ee
Aronunicargo <avinicius@unicargo.com.br>
2025-03-05 19:23:31 -0300

    feat: add app.get

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 require("dotenv").config();
 require("./jobs/syncJob");
 
@@ -6,10 +7,16 @@ const { syncReclamacoes } = require("./controllers/syncController");
 
 const app = express();
 
+const host = "0.0.0.0";
+
 syncReclamacoes()
   .then(() => console.log("Primeira chamada para atualizar as reclamações"))
   .catch((err) => console.error("Erro ao executar syncReclamacoes:", err));
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.send({ message: "Ai caliquinha" });
+});
+
+app.listen({ host, port: process.env.PORT || 3333 }, () => {
   console.log(`Server is running...`);
 });
